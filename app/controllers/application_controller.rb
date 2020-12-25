49b68bf22d4e208eded6f42c2fd27cddcb904272
Filipp Minayev <Filippmi@gmail.com>
2020-12-24 21:55:48 -0700

    commit tree aadfb800403fb929a30b13c9cf4593afcef73ecf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,45 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :'articles/index'
+  end
+
+  get '/articles/new' do
+    erb :'articles/new'
+  end
+
+  post '/articles' do
+    article = Article.create(params[:article])
+
+    redirect "/articles/#{article.id}"
+  end
+
+  get "/articles/:id/edit" do
+    find_article
+    erb :'articles/edit'
+  end
+  
+  get "/articles/:id" do
+    find_article
+    erb :'articles/show'
+  end
+  
+  patch "/articles/:id" do
+    find_article
+    article = Article.update(params[:article])
+    redirect "/articles/#{article.id}"
+  end
+
+  delete "/article/:id" do  
+    find_article
+    @article.destroy
+    redirect "/articles"
+  end
+
+  def find_article
+    @article = Article.find_by_id(params[:id])
+  end
 end
